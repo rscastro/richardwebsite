4ce7b15583cf1eb6b1787de5491e818d712e9f0f
Brian Forstat <brian@agilitygraphics.com>
2016-10-24 13:39:21 -0700

    Added redux to build.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import PeopleContainer from './components/PeopleContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <p>The app</p>
+        <PeopleContainer />
       </div>
     );
   }
