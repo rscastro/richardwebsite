a8c6e56997a54efb9386bb3ada9ffeedeca1772d
Brian Forstat <brian@agilitygraphics.com>
2016-10-24 14:23:57 -0700

    Added json to build.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import PeopleContainer from './components/PeopleContainer';
 
+const appData = require('./assets/data/data.json');
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <p>The app</p>
+        <h4>{appData.testCopy.title}</h4>
+        <p>{appData.testCopy.subtitle}</p>
         <PeopleContainer />
       </div>
     );
