e3ead11f269f29e75e3b397f930ab5bfc18f3ccf
Brian Forstat <brian@agilitygraphics.com>
2016-10-26 14:23:04 -0700

    Created ui-projects folder.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import PeopleContainer from './components/PeopleContainer';
+import PeopleContainer from './components/ui-projects/PeopleContainer';
 
 const appData = require('./assets/data/data.json');
 
