c9c61ec04a0d1220b16d0fe1a4cd5a3b63e7922f
Brian Forstat <brian@agilitygraphics.com>
2016-10-26 14:55:05 -0700

    Created Footer component

---
+++ src/components/ui-Footer/index.jsx
@@ -0,0 +1,14 @@
+import './index.scss';
+
+import React, { Component } from 'react';
+
+
+export default class Footer extends Component {
+	render() {
+		return (
+			<div className="footer">
+				<h3>Footer</h3>
+			</div>
+    	);
+  	}
+}
