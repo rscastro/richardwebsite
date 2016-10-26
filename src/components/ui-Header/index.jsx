8dde787551a7a821d3591562808916ef4570b773
Brian Forstat <brian@agilitygraphics.com>
2016-10-26 14:55:22 -0700

    Created Header component

---
+++ src/components/ui-Header/index.jsx
@@ -0,0 +1,14 @@
+import './index.scss';
+
+import React, { Component } from 'react';
+
+
+export default class Header extends Component {
+	render() {
+		return (
+			<div className="header">
+				<h3>Header</h3>
+			</div>
+    	);
+  	}
+}
