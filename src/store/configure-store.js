358ff6a97085d6c784d6f86bc113d8276badf9c9
Brian Forstat <brian@agilitygraphics.com>
2016-10-26 14:56:33 -0700

    Removed duplicate store file.

--- src/store/configure-store.js
+++
@@ -1,6 +0,0 @@
-import rootReducer from '../components/reducers-projects';
-import {createStore} from 'redux';
-
-export default (initialState) => {
-  return createStore(rootReducer, initialState);
-};
