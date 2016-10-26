15b33e9ddd943f65db3e3f8221ef4057268a4890
Brian Forstat <brian@agilitygraphics.com>
2016-10-26 14:55:38 -0700

    Created Hero component

---
+++ src/components/ui-Hero/index.jsx
@@ -0,0 +1,14 @@
+import './index.scss';
+
+import React, { Component } from 'react';
+
+
+export default class Hero extends Component {
+	render() {
+		return (
+			<div className="hero">
+				<h3>Hero</h3>
+			</div>
+    	);
+  	}
+}
