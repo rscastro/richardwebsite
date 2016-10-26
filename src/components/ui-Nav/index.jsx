c6fa63ac03d84c06d7efeff2e17db79dc5eac6b4
Brian Forstat <brian@agilitygraphics.com>
2016-10-26 14:56:09 -0700

    Created Nav component

---
+++ src/components/ui-Nav/index.jsx
@@ -0,0 +1,14 @@
+import './index.scss';
+
+import React, { Component } from 'react';
+
+
+export default class Nav extends Component {
+	render() {
+		return (
+			<div className="nav">
+				<h3>Nav</h3>
+			</div>
+    	);
+  	}
+}
