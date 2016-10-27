9b7f22f2a8c9b487a28569e185ece1086d31ceeb
Brian Forstat <brian@agilitygraphics.com>
2016-10-27 15:40:17 -0700

    Added data to footer and rendering out the nav.

--- src/components/ui-Footer/index.jsx
+++ src/components/ui-Footer/index.jsx
@@ -3,10 +3,38 @@ import './index.scss';
 import React, {Component} from 'react';
 
 export default class Footer extends Component {
+
+  static get propTypes() {
+    return {data: React.PropTypes.object};
+  }
+
+  renderFooterNav(links) {
+    return links.map((link, index) => {
+      return (
+        <li key={index}>
+          <a href={link.url}>{link.title}</a>
+        </li>
+      );
+    });
+  }
+
   render() {
+    const {
+      data: {
+        copyright,
+        footerNav
+      }
+    } = this.props;
+
+    console.log('footerNav', footerNav);
+
     return (
       <div className="footer">
           <h3>Footer</h3>
+          <ul className="footer-nav">
+            { this.renderFooterNav(footerNav) }
+          </ul>
+          <p>{copyright}</p>
       </div>
     );
   }
