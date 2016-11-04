9af9aee1b3d8495e1b2cc9da3b02014f9f341dfc
Brian Forstat <brian@agilitygraphics.com>
2016-11-04 10:56:45 -0700

    Set up for icon to be animated.

--- src/components/ui-HamburgerIcon/index.jsx
+++ src/components/ui-HamburgerIcon/index.jsx
@@ -2,19 +2,24 @@ import './index.scss';
 
 import React, { Component } from 'react';
 import TweenLite from 'gsap'
-import hamburgerSvg from '../../assets/images/hamburger.svg';
 
 export default class HamburgerIcon extends Component {
 
   onClick() {
-    TweenLite.to('.hamburger-svg', 1, {opacity: .2});
+    TweenLite.to('#l1', .2, {rotation:-45, transformOrigin:'100% 50%'});
+    TweenLite.to('#l2', .2, {rotation:45, transformOrigin:'50% 50%'});
+    TweenLite.to('#l3', .2, {rotation:-45, transformOrigin:'0% 50%'});
   }
 
   render() {
     return (
       <div className="hamburger-icon" onClick={this.onClick}>
         <a href="#">
-          <img src={hamburgerSvg} alt="Hamburger" className="hamburger-svg" />
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="hamburger-svg">
+            <rect id="l1" y="0" width="32" height="2"/>
+            <rect id="l2" y="15" width="32" height="2"/>
+            <rect id="l3" y="30" width="32" height="2"/>
+          </svg>
         </a>
       </div>
     );
