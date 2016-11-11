c71723f006ee3b5749c2d8f8990fd7735eed94f0
Brian Forstat <brian@agilitygraphics.com>
2016-11-11 08:59:08 -0800

    Fixed alignment issue of titles that showed up after breaking things into pages.

--- src/components/ui-SectionHeader/index.jsx
+++ src/components/ui-SectionHeader/index.jsx
@@ -22,7 +22,7 @@ export default class SectionHeader extends Component {
         <div className="wrapper">
           <div className={ number ? 'grid' : 'grid grid--justify-center' }>
             { number ?
-              <div className="grid__col-xs-12 grid__col-sm-2 number">
+              <div className="grid__col-xs-12 grid__col-sm-2 number-hldr">
                 <h4 className="nunber-title underline">{number}</h4>
               </div>
             : null }
