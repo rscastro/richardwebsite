1b0b99c0b83a66979abb5eb4ac576b4ef392b10a
Brian Forstat <brian@agilitygraphics.com>
2016-11-11 15:09:55 -0800

    Added function to render out each projects components specified by json.

--- src/components/ui-Work/index.jsx
+++ src/components/ui-Work/index.jsx
@@ -13,16 +13,46 @@ import Carousel from '../ui-Carousel';
 import WorkGrid from '../ui-WorkGrid';
 
 
+console.log('pageContent:', workData.work.projects[0].pageContent);
+
 export default class Work extends Component {
+
+  // let currentProject = 1;
+
+  renderPageComponents(pageContent) {
+    return pageContent.map((component, index) => {
+      if (component.type === 'projectHero') {
+        return (
+          <ProjectHero key={component.type + '-' + index} data={component} />
+        );
+      } else if (component.type === 'projectIntro') {
+        return (
+          <ProjectIntro key={component.type + '-' + index} data={component} />
+        );
+      } else if (component.type === 'sectionHeader') {
+        return (
+          <SectionHeader key={component.type + '-' + index} data={component} />
+        );
+      } else if (component.type === 'textColumns') {
+        return (
+          <TextColumns key={component.type + '-' + index} data={component} />
+        );
+      }
+    });
+  }
+
   render() {
     return (
       <div className="work">
         <div className="wrapper">
-          <ProjectHero data={workData.projectHero} />
+          
+          { this.renderPageComponents(workData.work.projects[0].pageContent) }
+
+          {/* <ProjectHero data={workData.projectHero} />
           <SectionHeader data={workData.sectionHeader1} />
           <ProjectIntro data={workData.work.projects[1]} />
           <SectionHeader data={workData.sectionHeader2} />
-          <TextColumns data={workData.textColumns} />
+          <TextColumns data={workData.textColumns} /> */}
           <Image data={workData.img} />
 
           <Video data={workData.video} />
