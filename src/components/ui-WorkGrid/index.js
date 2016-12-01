cb1ba144914e5121e606ab5d38bf052e96316aa6
Brian Forstat <brian@agilitygraphics.com>
2016-12-01 10:23:20 -0800

    Thumb animation.

--- src/components/ui-WorkGrid/index.js
+++ src/components/ui-WorkGrid/index.js
@@ -2,24 +2,12 @@ import './_index.scss';
 
 import React, {PropTypes, Component} from 'react';
 
-// Alias includes in the webpack.config files
-import TweenLite from 'TweenLite';
-
 export default class WorkGrid extends Component {
 
   static get propTypes() {
     return {
       projects: React.PropTypes.array
     };
-  }
-
-  onMouseEnterHandler(event) {
-    TweenLite.to(event.currentTarget.querySelector('img'), 3, {scale:1.05, transformOrigin:'50% 50%'});
-  }
-
-  onMouseLeaveHandler(event) {
-    // console.log('this:', event.currentTarget);
-    TweenLite.to(event.currentTarget.querySelector('img'), 3, {scale:1, transformOrigin:'50% 50%'});
   }
 
   renderProjects(projects) {
@@ -43,8 +31,6 @@ export default class WorkGrid extends Component {
           <a
             href={project.url}
             className="workgrid__item-link"
-            onMouseEnter={this.onMouseEnterHandler}
-            onMouseLeave={this.onMouseLeaveHandler}
           >
             <img src={project.img} alt={project.title} className="workgrid__item-img" />
             <div className="workgrid__item-copy">
