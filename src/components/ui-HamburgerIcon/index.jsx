da51ab4688c9356701014952bee5e7b9a738cf1d
Brian Forstat <brian@agilitygraphics.com>
2016-12-05 15:34:27 -0800

    Removing commented out over and out events handlers.

--- src/components/ui-HamburgerIcon/index.jsx
+++ src/components/ui-HamburgerIcon/index.jsx
@@ -44,26 +44,12 @@ export default class HamburgerIcon extends Component {
     this.props.onToggleNav();
   }
 
-  // onMouseEnterHandler() {
-  //   if (this.props.navOpen) {
-  //     TweenLite.to('#l2', .2, {scaleX:1.2, transformOrigin:'50% 50%'});
-  //   } else {
-  //     TweenLite.to('#l2', .2, {scaleX:.7, transformOrigin:'50% 50%'});
-  //   }
-  // }
-  //
-  // onMouseLeaveHandler() {
-  //   TweenLite.to('#l2', .2, {scaleX:1, transformOrigin:'50% 50%'});
-  // }
-
   render() {
     return (
       <div className="hamburger-icon">
         <a
           href="#"
           onClick={this.onClickHandler.bind(this)}
-          // onMouseEnter={this.onMouseEnterHandler.bind(this)}
-          // onMouseLeave={this.onMouseLeaveHandler.bind(this)}
         >
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 30" className="hamburger-svg">
             <rect id="l1" y="0" width="48" height="1" fill="#fff" />
