8500771c6a3d106690411d70b8b60865ac2f6545
Brian Forstat <brian@agilitygraphics.com>
2016-12-16 16:21:37 -0800

    Start to new headers scrolling.

--- src/components/ui-Lab/index.jsx
+++ src/components/ui-Lab/index.jsx
@@ -5,6 +5,7 @@ const labsData = require('../../assets/data/labs.json');
 import React, { Component } from 'react';
 import PageContent from '../ui-PageContent';
 import NavFooter from '../ui-NavFooter';
+import Hero from '../ui-Hero';
 
 export default class Lab extends Component {
 
@@ -30,10 +31,21 @@ export default class Lab extends Component {
   }
 
   render() {
-    const { currentLab } = this.state;
+    const {
+      currentLab,
+      currentLab: {
+        pageHero
+      }
+    } = this.state;
 
     return (
       <div className="lab">
+        <Hero
+          src={ pageHero.src }
+          copy={ pageHero.copy }
+          projectColor={ currentLab.projectColor }
+          headerOverlayOpacity={ pageHero.headerOverlayOpacity || 0.5 }
+        />
         <PageContent currentProject={ currentLab } />
         <NavFooter />
       </div>
